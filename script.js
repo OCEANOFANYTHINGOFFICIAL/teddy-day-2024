5a8611a2f0db2b6fa2dcbaa7dc68e6b300bd925c
OCEAN OF ANYTHING <oceanofanything@gmail.com>
2024-02-09 16:27:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+setTimeout(function() {
+    document.getElementById('teddy-button-to').addEventListener('click', function() {
+        window.location.href = 'teddy-1/';
+    });
+}, 15000); // 15 seconds delay
+
+
+const myPopup = new Popup({
+    id: "popup-1",
+    widthMultiplier: 0.5,
+    title: "Secret Message !!!",
+    content: `Click On The Teddy's belly After Reading The Letter To Get A Surprise 🥀`,
+    backgroundColor: "hotpink",
+    titleColor: "white",
+    textColor: "white",
+    borderRadius: "35px",
+    borderColor: "white",
+    borderWidth: "3px",
+    font: "Roboto",
+    css: `@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');`,
+    showImmediately: true,
+});
